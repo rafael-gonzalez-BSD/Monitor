b6e7a91b93a3d4755e2b250d45d001265adf0aa4
Manuel Gilberto León Villegas <manuelgilbertoleonvillegas@gmail.com>
2019-12-03 13:08:06 -0600

    Se integro la cintilla. se diseño la cintilla. se integro el idioma de la tabla.

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,6 +1,32 @@
 import { NgModule } from '@angular/core';
+import { getLangPaginatorIntl } from './components/inventario/procesos/grilla-proceso/grilla-proceso-lang';
 
 import {
+  MatButtonModule,
+  MatMenuModule,
+  MatToolbarModule,
+  MatCardModule,
+  MatDialogModule,
+  MatIconModule,
+  MatGridListModule,
+  MatDividerModule,
+  MatSlideToggleModule,
+  MatSidenavModule,
+  MatListModule,
+  MatTreeModule,
+  MatTabsModule,
+  MatSelectModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatTableModule,
+  MatPaginatorModule,
+  MatPaginatorIntl,
+  MatSortModule,
+  MatPaginator
+} from '@angular/material';
+
+@NgModule({
+  imports: [
     MatButtonModule,
     MatMenuModule,
     MatToolbarModule,
@@ -20,50 +46,28 @@ import {
     MatTableModule,
     MatPaginatorModule,
     MatSortModule
- } from '@angular/material';
-
-@NgModule({
-    imports: [
-        MatButtonModule,
-        MatMenuModule,
-        MatToolbarModule,
-        MatCardModule,
-        MatDialogModule,
-        MatIconModule,
-        MatGridListModule,
-        MatDividerModule,
-        MatSlideToggleModule,
-        MatSidenavModule,
-        MatListModule,
-        MatTreeModule,
-        MatTabsModule,
-        MatSelectModule,
-        MatFormFieldModule,
-        MatInputModule,
-        MatTableModule,
-        MatPaginatorModule,
-        MatSortModule
-    ],
-    exports: [
-        MatButtonModule,
-        MatMenuModule,
-        MatToolbarModule,
-        MatCardModule,
-        MatDialogModule,
-        MatIconModule,
-        MatGridListModule,
-        MatDividerModule,
-        MatSlideToggleModule,
-        MatSidenavModule,
-        MatListModule,
-        MatTreeModule,
-        MatTabsModule,
-        MatSelectModule,
-        MatFormFieldModule,
-        MatInputModule,
-        MatTableModule,
-        MatPaginatorModule,
-        MatSortModule
-    ]
+  ],
+  exports: [
+    MatButtonModule,
+    MatMenuModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatDialogModule,
+    MatIconModule,
+    MatGridListModule,
+    MatDividerModule,
+    MatSlideToggleModule,
+    MatSidenavModule,
+    MatListModule,
+    MatTreeModule,
+    MatTabsModule,
+    MatSelectModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule
+  ],
+  providers: [{ provide: MatPaginatorIntl, useValue: getLangPaginatorIntl() }]
 })
-export class MaterialModule{}
+export class MaterialModule {}
